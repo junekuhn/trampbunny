b19703d04990c12a6a01787300bf15c8b899abdf
Justin Kuhn <justintkuhn@gmail.com>
2023-02-16 20:32:13 +0000

    first build

---
+++ docs/internal/immutable/chunks/2-ca7d93d5.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-d23e61b0.js";export{t as component};
--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,26 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from "@sveltejs/adapter-static";
+const dev = process.argv.includes('dev');
 
-/** @type {import('@sveltejs/kit').Config} */
+// /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+      // default options are shown. On some platforms
+      // these options are set automatically — see below
+      pages: 'docs',
+      assets: 'docs',
+      fallback: null,
+      precompress: false,
+      strict: true,
+	//   trailingSlash: 'always',
+		}),
+		paths: {
+			base: dev ? '' : '/trampbunny',
+		  },
+		  // If you are not using a .nojekyll file, change your appDir to something not starting with an underscore.
+		  // For example, instead of '_app', use 'app_', 'internal', etc.
+		  appDir: 'internal',
 	}
 };
 
