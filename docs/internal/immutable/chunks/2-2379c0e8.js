e712c5f1d22abdefefdace08d08c81861d18c0fd
Justin Kuhn <justintkuhn@gmail.com>
2023-02-16 21:12:40 +0000

    hydra is in

---
+++ docs/internal/immutable/chunks/2-2379c0e8.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-ff789a94.js";export{t as component};
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,16 @@
+
+
+<script>
+	import { onMount } from 'svelte';
+          // create a new hydra-synth instance
+          onMount(() => {
+            var hydra = new Hydra({ detectAudio: false })
+      osc(4, 0.1, 1.2).out()
+          });
+
+</script>
+
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
