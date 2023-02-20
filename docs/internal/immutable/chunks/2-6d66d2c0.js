42eb98b4cf0d398ba380bb44c88391dbb2623ba0
Justin Kuhn <justintkuhn@gmail.com>
2023-02-20 15:45:42 +0000

    mobile opts and favicon

--- docs/internal/immutable/chunks/2-6d66d2c0.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-f80f262d.js";export{t as component};
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,13 +7,15 @@
   <h1>trampbunny</h1>
   <h2>live-coded music loops and pixel soups</h2>
   <ul>
-    <li><button>twitch</button></li>
-    <li><button>instagram</button></li>
-    <li><button>demo reel</button></li>
+    <li><a href="https://twitch.tv/trampbunny"><button>twitch</button></a></li>
+    <li><a href="https://instagram.com/trampbunny_"><button>instagram</button></a></li>
+    <li><a href="https://youtu.be/M5gzqi69yh8"><button>demo reel</button></a></li>
+    <li><a href="mailto:trampbunny@proton.me"><button>contact</button></a></li>
   </ul>
 </main>
 
 <style>
+
   main {
     background-color: rgba(0, 0, 0, 0.5);
     width: 100%;
@@ -22,41 +24,62 @@
     text-align: center;
   }
 
+  h1, h2 {
+    color: #C495FF;
+   text-shadow: -6px 5px black;
+  }
+
   h1 {
     font-size: 50px;
     font-family: '2Peas';
-    color: #C495FF;
+
   }
 
   h2 {
     font-size: 15px;
     font-family: 'kidsplay';
-    color: #C495FF;
   }
- ul {
-  list-style-type: none;
-  display: flex;
-  flex-direction: column;
-  width: 100%;
-  padding: 0px;
+  
+  ul {
+    list-style-type: none;
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    padding: 0px;
 
- }
+  }
 
  li {
-  margin-top: 20px;
+  margin-top: 30px;
  }
 
 
 
 
  button {
-  background-color: black;
+  background-color: rgba(56, 56, 56, 0.5);
   color: white;
   padding: 10px;
   border-radius: 5px;
   font-family: 'kidsplay';
   font-size: 20px;
   width: 200px;
+  box-shadow: -6px 5px black;
+ }
+
+ @media (min-width:750px) {
+    h1 {
+      font-size: 100px;
+    }
+
+    h2 {
+      font-size: 25px;
+    }
+
+    button {
+      width: 300px;
+      font-size: 25px;
+    }
  }
 
 </style>
